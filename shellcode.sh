d1ca9f786747f54c0ff7d31aa034dca56038f36d
piyoma <249300887@qq.com>
2020-02-07 08:14:33 +0800

    .

--- shellcode.sh
+++ shellcode.sh
@@ -232,7 +232,7 @@ function install_ss(){
     cd $cur_dir
     # unzip -q manyuser.zip
     # mv shadowsocks-manyuser/shadowsocks /usr/local/
-	git clone https://github.com/qcgzxw/ssr.git /usr/local/shadowsocks
+	git clone https://github.com/piyoma/ssr.git /usr/local/shadowsocks
     if [ -f /usr/local/shadowsocks/server.py ]; then
         chmod +x /etc/init.d/shadowsocks
         # Add run on system start up
