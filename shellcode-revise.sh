9f08eac6d1a0fb9d3488693989189e8b6aee00f1
piyoma <249300887@qq.com>
2020-06-12 22:06:36 +0800

    Update shellcode-revise.sh

--- shellcode-revise.sh
+++ shellcode-revise.sh
@@ -137,12 +137,12 @@ function download_files(){
     # fi
     # Download ShadowsocksR chkconfig file
     if [ "$OS" == 'CentOS' ]; then
-        if ! wget --no-check-certificate https:/raw.githubusercontent.com/piyoma/degenerate-forsaken/master/shadowsocksR -O /etc/init.d/shadowsocks; then
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/piyoma/degenerate-forsaken/master/shadowsocksR -O /etc/init.d/shadowsocks; then
             echo "Failed to download ShadowsocksR chkconfig file!"
             exit 1
         fi
     else
-        if ! wget --no-check-certificate https:/raw.githubusercontent.com/piyoma/degenerate-forsaken/master/shadowsocksR-debian -O /etc/init.d/shadowsocks; then
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/piyoma/degenerate-forsaken/master/shadowsocksR-debian -O /etc/init.d/shadowsocks; then
             echo "Failed to download ShadowsocksR chkconfig file!"
             exit 1
         fi
